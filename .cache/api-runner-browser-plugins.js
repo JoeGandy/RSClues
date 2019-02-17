bd68e00f9c5e6dcc3aad4aec36f34200277ca81c
Joe Gandy <joeg@adaptive.co.uk>
2019-02-17 09:15:09 +0000

    Removing ignored files

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/joe/projects/RSClues/node_modules/gatsby-plugin-google-analytics/gatsby-browser.js'),
-      options: {"plugins":[],"trackingId":"UA-49984946-3"},
-    },{
-      plugin: require('/home/joe/projects/RSClues/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
