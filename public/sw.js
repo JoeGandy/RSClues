32cfe6af5b756dd13f08bc0b94424c8af452f119
Joe Gandy <joeg@adaptive.co.uk>
2019-02-17 10:18:22 +0000

    Fixed accessiblity issues with links

--- public/sw.js
+++ public/sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-80f10ea90962422247ed.js"
+    "url": "webpack-runtime-e35f9cdada4c64a1a168.js"
   },
   {
     "url": "app-93acd2b86477b0634d78.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "683b6ddf5bd41586a3a0dd4ff3cbc37f"
+    "revision": "82814094527cbc89bffe81530a37b6bf"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
--- src/components/header.js
+++ src/components/header.js
@@ -8,18 +8,18 @@ export default class Header extends Component {
             <div className="_header">
                 <div className="_logo"><a href="/">RSClues</a></div>
                 <div className="_links">
-                    <a href="/"
+                    <a href="/" title="Clue Solver"
                        className={window_is_alive && window.location.pathname === "/" ? "active" : ""}>
-                        <FaSearch/>
+                        <FaSearch attr={{alt: 'Clue Solver', title: 'Clue Solver'}}/>
                         <span>Clue Solver</span>
                     </a>
-                    <a href="/about"
+                    <a href="/about" title="What is this"
                        className={window_is_alive && (window.location.pathname === "/about" || window.location.pathname === "/about/") ? "active" : ""}>
-                        <FaQuestion/>
+                        <FaQuestion attr={{alt: 'What is this', title: 'What is this'}}/>
                         <span>What is this</span></a>
-                    <a href="/changelog"
+                    <a href="/changelog" title="Changelog"
                        className={window_is_alive && (window.location.pathname === "/changelog" || window.location.pathname === "/changelog/") ? "active" : ""}>
-                        <FaBook/>
+                        <FaBook  attr={{alt: 'Changelog', title: 'Changelog'}}/>
                         <span>Changelog</span></a>
                 </div>
             </div>
